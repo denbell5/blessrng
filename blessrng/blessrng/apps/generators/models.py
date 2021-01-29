4dfacb3ab45067c081b8bccbdf0a90503dc1494e
ForPuller <denonec@gmail.com>
2021-01-29 23:46:25 +0200

    Add Generated_Value model

--- blessrng/blessrng/apps/generators/models.py
+++ blessrng/blessrng/apps/generators/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Generated_Value(models.Model):
+    value = models.IntegerField('value')
+    generated_at = models.DateTimeField('date published')
