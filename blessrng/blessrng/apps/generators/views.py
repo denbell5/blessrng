323564e4b8e0d07116f058ebe84a152028d79484
denbell <denbell@ukr.net>
2021-01-30 01:58:00 +0200

    update generate views

--- blessrng/blessrng/apps/generators/views.py
+++ blessrng/blessrng/apps/generators/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from random import randint
+from generators.models_dir.dto.dto import RandomIntDto
 # Create your views here.
 
-def index(request):
+def _get_randint_response(request, dto: RandomIntDto):
     template = loader.get_template('generators/index.html')
-    context = {}
+    context = {'random_int_dto': dto}
     return HttpResponse(template.render(context, request))
+
+def index(request):
+    dto = RandomIntDto(None, None, None)
+    return _get_randint_response(request, dto)
+
+def generate(request):
+    floor = int(request.POST['floor'])
+    ceiling = int(request.POST['ceiling'])
+    value = randint(floor, ceiling)
+    dto = RandomIntDto(floor, ceiling, value)
+    return _get_randint_response(request, dto)
