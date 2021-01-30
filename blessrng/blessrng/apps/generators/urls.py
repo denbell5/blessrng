58092ea37cce70955951dd53cb3369fb9bc535a0
denbell <denbell@ukr.net>
2021-01-30 21:27:57 +0200

    rename integer view

--- blessrng/blessrng/apps/generators/urls.py
+++ blessrng/blessrng/apps/generators/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'generators'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('generate', views.generate, name='generate'),
+    path('', views.integer, name='integer'),
+    path('integer', views.integer, name='integer'),
     path('generate_pass', views.generate_pass, name='generate_pass'),
 ]
--- blessrng/blessrng/apps/generators/views.py
+++ blessrng/blessrng/apps/generators/views.py
@@ -4,18 +4,29 @@ from django.template import loader
 from random import randint
 from generators.models_dir.dto.dto import *
 from generators.models_dir.random_generators.password_generators import generate_password
+from generators.models import *
 # Create your views here.
 
+
 def _get_randint_response(request, dto: RandomIntDto):
-    template = loader.get_template('generators/index.html')
+    template = loader.get_template('generators/integer.html')
     context = {'random_int_dto': dto}
     return HttpResponse(template.render(context, request))
 
-def index(request):
+
+def integer(request):
+    if request.method == 'GET':
+        return __integer_get(request)
+    elif request.method == 'POST':
+        return __integer_post(request)
+
+
+def __integer_get(request):
     dto = RandomIntDto(None, None, None)
     return _get_randint_response(request, dto)
 
-def generate(request):
+
+def __integer_post(request):
     floor = int(request.POST['floor'])
     ceiling = int(request.POST['ceiling'])
     value = randint(floor, ceiling)
@@ -30,4 +41,3 @@ def generate_pass(request):
     template = loader.get_template('generators/index.html')
     context = {'password_dto': dto}
     return HttpResponse(template.render(context, request))
-
