4c6f853b01a86dd358b9bdf085cd6852d7289dc3
denbell <denbell@ukr.net>
2021-01-30 18:47:47 +0200

    implement google auth

--- blessrng/blessrng/blessrng/urls.py
+++ blessrng/blessrng/blessrng/urls.py
@@ -7,7 +7,8 @@ from django.urls import path, include
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
-
+from django.contrib.auth import logout
+from django.conf import settings
 
 urlpatterns = [
     path('', include('generators.urls')),
@@ -19,14 +20,17 @@ urlpatterns = [
          (
              template_name='app/login.html',
              authentication_form=forms.BootstrapAuthenticationForm,
-             extra_context=
-             {
+             extra_context={
                  'title': 'Log in',
-                 'year' : datetime.now().year,
+                 'year': datetime.now().year,
              }
          ),
          name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
     path('generators/', include('generators.urls')),
+    path('', include('social_django.urls', namespace='social')),
+    path('logout/', logout,
+         {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
+
 ]
