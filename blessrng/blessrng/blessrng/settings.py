c6e50ca6d025e7398a4ac0df9db2a8b688662b34
denbell <denbell@ukr.net>
2021-01-31 18:43:11 +0200

    use DATABASES config from config.py

--- blessrng/blessrng/blessrng/settings.py
+++ blessrng/blessrng/blessrng/settings.py
@@ -95,12 +95,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'blessrng.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = sensitive_config.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
