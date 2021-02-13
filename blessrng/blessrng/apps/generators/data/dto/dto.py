d57d2ceb120e15d6cfe8d71c60c98d7a12a5c8ab
denbell <denbell@ukr.net>
2021-02-13 21:41:32 +0200

    Add logic to generate int sets

--- blessrng/blessrng/apps/generators/data/dto/dto.py
+++ blessrng/blessrng/apps/generators/data/dto/dto.py
@@ -1,14 +1,21 @@
+from typing import List
+
+from django.http.request import HttpRequest
+
+
 class RandomIntDto:
     class Const:
         dto_name = 'random_int_dto'
+        count_name = 'count'
         floor_name = 'floor'
         ceiling_name = 'ceiling'
         value_name = 'value'
 
-    def __init__(self, floor, ceiling, value):
+    def __init__(self, count: int, floor: int, ceiling: int, values: List[int]):
+        self.count = count
         self.floor = floor
         self.ceiling = ceiling
-        self.value = value
+        self.values = values
 
 
 class RandomPasswordDto:
