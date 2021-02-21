706dc932783ab6d393ea222e2b624c1ce63d1c05
denbell <denbell@ukr.net>
2021-02-21 19:40:52 +0200

    Add animation for clipboard icon

--- blessrng/blessrng/apps/app/static/app/scripts/clipboard.js
+++ blessrng/blessrng/apps/app/static/app/scripts/clipboard.js
@@ -1,4 +1,10 @@
-function copyToClipboardFrom(resultContainerId, resultElementTag) {
+function copyToClipboardFrom(clickedElement, resultContainerId, resultElementTag) {
+    const oldIcon = clickedElement.getElementsByTagName('svg')[0];
+
+    const icon = oldIcon.cloneNode(true);
+    oldIcon.parentNode.replaceChild(icon, oldIcon);
+    icon.classList.add('copy-icon-mouse-up');
+
     const resultContainer = document.getElementById(resultContainerId);
     var text = '';
     var upperCaseTagName = resultElementTag.toUpperCase();
