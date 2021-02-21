c0727effed34e5aadb4363bb2b2c43483c240065
denbell <denbell@ukr.net>
2021-02-21 19:54:19 +0200

    Add animation to form refresh

--- blessrng/blessrng/apps/app/static/app/scripts/form-reset.js
+++ blessrng/blessrng/apps/app/static/app/scripts/form-reset.js
@@ -1,8 +1,13 @@
-function resetForm(id) {
+function resetForm(clickedElement, id) {
     const form = document.getElementById(id);
     if (!form) {
         return;
     }
+
+    const oldIcon = clickedElement.getElementsByTagName('svg')[0];
+    const icon = oldIcon.cloneNode(true);
+    oldIcon.parentNode.replaceChild(icon, oldIcon);
+    icon.classList.add('refresh-icon-mouse-up');
 
     const controls = form.querySelectorAll('input, textarea');
     controls.forEach(el => {
