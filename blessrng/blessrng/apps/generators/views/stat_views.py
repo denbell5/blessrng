8055850a1e20cb368ca3a3cdaaf43614e0e13018
denone <denonec@gmail.com>
2021-05-30 22:09:02 +0300

    add site and user statistics

--- blessrng/blessrng/apps/generators/views/stat_views.py
+++ blessrng/blessrng/apps/generators/views/stat_views.py
@@ -11,16 +11,40 @@ from datetime import datetime, timezone
 from generators.data.dto.stat_dto import *
 
 
-def __create_stat_response(request: HttpRequest, dto: StatDto):
-    template = loader.get_template('generators/stat.html')
-    context = {'dto': dto}
-    return HttpResponse(template.render(context, request))
 
 
 def stat(request: HttpRequest):
     # get all stat and create StatDto from it
     latest = get_latest_stats()
-    dto = StatDto(
-        latest=latest
-    )
+
+    int_gen_count = RandIntSet.objects.count() 
+    pwd_gen_count = RandPwdSet.objects.count()
+    word_gen_count = RandWordSet.objects.count()
+
+    total_gen_count = int_gen_count + pwd_gen_count + word_gen_count
+    
+    anon_gen_count = RandIntSet.objects.filter(user_id = None).count() + RandPwdSet.objects.filter(user_id = None).count() + RandWordSet.objects.filter(user_id = None).count()
+    registered_gen_count = total_gen_count - anon_gen_count
+
+    registered_users = User.objects.count()
+    avg_gen_per_user = registered_gen_count / registered_users
+
+    site_stat_dto = SiteStatDto(int_gen_count, pwd_gen_count, word_gen_count, total_gen_count, anon_gen_count, registered_gen_count, registered_users, avg_gen_per_user)
+    
+    # get user stat 
+    usr_id = request.user.id
+    int_gen_count = RandIntSet.objects.filter(user_id = usr_id).count() 
+    pwd_gen_count = RandPwdSet.objects.filter(user_id = usr_id).count()
+    word_gen_count = RandWordSet.objects.filter(user_id = usr_id).count()
+    total_gen_count = int_gen_count + pwd_gen_count + word_gen_count
+    user_stat_dto = UserStatDto(int_gen_count, pwd_gen_count, word_gen_count, total_gen_count)
+
+    dto = StatDto(siteStat = site_stat_dto, userStat = user_stat_dto, latest=latest)
+    
     return __create_stat_response(request, dto)
+
+
+def __create_stat_response(request: HttpRequest, dto: StatDto):
+    template = loader.get_template('generators/stat.html')
+    context = {'dto': dto}
+    return HttpResponse(template.render(context, request))
